3f0d0afbff1ea2056f54431f6ebde9b0cc9e3ef0
deepanshut041 <deepanshut041@gmail.com>
2017-11-08 16:37:47 +0530

    Main, Patient, Register

---
+++ app/src/main/java/com/example/deepanshu/patient/PatientDetailActivity.java
@@ -0,0 +1,13 @@
+package com.example.deepanshu.patient;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PatientDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_detail);
+    }
+}
